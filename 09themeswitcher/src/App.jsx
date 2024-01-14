415554e335b78c49f334ccfa298f363c8034cb0d
Mohamed Ijlal <mohamedijlal27@gmail.com>
2024-01-14 12:26:09 +0530

    themeswitcher using context

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="bg-green-700">Theme Switcher</h1>
+    </div>
+  );
+}
+
+export default App;
